a09f568f37c8b97697f35e66e1830f39fed8a3eb
TahmidReza143 <tahmidreza89@gmail.com>
2019-06-12 16:50:06 +0600

    we have merged another to master

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Vs Code is Awesome...");
     }
+    
     public static int area(int x, int y, int z){
         return x*y*z;
     }
